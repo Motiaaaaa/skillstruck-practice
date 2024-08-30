62e1ea6c9a9895e0091e60322d7ddf8edf8a2058
Motiaa <motiaa.radwan2008@gmail.com>
2024-08-30 12:50:34 -0700

    exported from Skill Struck

---
+++ Removing_from_Lists_in_Python.py
@@ -0,0 +1,19 @@
+# harvest = ["pumpkins", "apples", "corn", "squash", "carrots"]
+# harvest.remove("squash")
+# harvest.pop(1)
+# print(harvest)
+
+#Challenge1
+# choice = input("What skill do you want to remove?")
+
+# skills = ["Piano", "Soccer", "Coding", "Cooking", "Writing"]
+
+# skills.remove(choice)
+
+# print(skills)
+
+
+#Challenge2
+place3 = input("Creat a list of names:").split()
+bronze = place3.pop(2)
+print(bronze + " got the bronze medal")
