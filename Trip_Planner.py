958836c985e8177b0b49e333b855ad220e724d23
Motiaa <motiaa.radwan2008@gmail.com>
2024-08-30 12:50:33 -0700

    exported from Skill Struck

---
+++ Trip_Planner.py
@@ -0,0 +1,4 @@
+num1 = int(input("number of miles your car can drive in a day"))
+num2 = int(input("total number of miles to get to your destination"))
+total = num2/num1
+print("The total number of days you will need to drive there is " + str(total))
