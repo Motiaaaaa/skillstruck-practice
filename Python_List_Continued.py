8c1f2dd514372ab632fd454bf311afb09f563093
Motiaa <motiaa.radwan2008@gmail.com>
2024-08-30 12:50:35 -0700

    exported from Skill Struck

---
+++ Python_List_Continued.py
@@ -0,0 +1,23 @@
+# smells = ["skunk", "lilac","rain","ocean","garbage","cleaner","cookies"]
+# print(smells[0:3])
+# smells[6] = "perfume"
+# print(smells)
+# print(len(smells))
+
+
+#Challenge1
+# my_list = [int(n) for n in input().split()]
+# biggest = 0
+# for num in my_list:
+# 	if num > biggest:
+# 		biggest = num
+
+# print(biggest)
+my_list = [int(n) for n in input().split()]
+
+current = my_list[0]
+
+for num in my_list:
+	if num > current:
+		print(my_list)
+	current = 
