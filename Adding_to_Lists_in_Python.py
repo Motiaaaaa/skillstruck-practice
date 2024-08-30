531783d31a445c7648e71dadafac36e826b213f8
Motiaa <motiaa.radwan2008@gmail.com>
2024-08-30 12:50:34 -0700

    exported from Skill Struck

---
+++ Adding_to_Lists_in_Python.py
@@ -0,0 +1,25 @@
+# flowers = ["rose", "tulip", "lilac", "sunflower"]
+# flowers.append("lilly")
+# flowers.extend(["lilly","carnation","orchid"])
+# flowers.insert(2,"lilly")
+
+
+#Challenge 1
+# treats = ["popcorn", "popsicles", "soda", "chips", "cookies"]
+
+# one_treat = input("One treat you'd like to add to the list:")
+
+# more_treats = input("Another treat you'd like to add to the list:")
+
+# treats.append(one_treat)
+# treats.append(more_treats)
+
+# print(treats)
+
+
+
+#Challenge 2
+events = ["Swimming","Track","Gymnastics","Cycling","sports"]
+new_events = ["skateboarding","surfing","sport climbing"]
+events.append(new_events)
+print(events)
