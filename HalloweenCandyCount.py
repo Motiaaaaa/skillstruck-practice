5d2c9d8a2f28f5b72f590f6bc3360444dc2af4dc
Motiaa <motiaa.radwan2008@gmail.com>
2024-08-30 12:50:31 -0700

    exported from Skill Struck

---
+++ HalloweenCandyCount.py
@@ -0,0 +1,9 @@
+snickers = int(input("How many snickers you got?"))
+
+nerds= int(input("How many nerds you got?"))
+
+butterfingers = int(input("How many butterfingers you got?"))
+
+total = snickers + nerds + butterfingers
+
+print("This year, you got " + str(snickers) + " snickers, " + str(nerds) + " nerds, and " + str(butterfingers) + " butterfingers. The total number of these candies is " + str(total) + " candies.")
