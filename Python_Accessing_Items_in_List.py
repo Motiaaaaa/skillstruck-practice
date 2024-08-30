c652d6b8b12a817af2db1a3d90e2ee0d388f7e3c
Motiaa <motiaa.radwan2008@gmail.com>
2024-08-30 12:50:33 -0700

    exported from Skill Struck

---
+++ Python_Accessing_Items_in_List.py
@@ -0,0 +1,16 @@
+# colors = ["red", "orange", "yellow", "green", "blue"]
+# print(colors[2])
+# print("I love the color " + colors[2] + ".")
+
+##Challenge 1
+
+# numbers = [int(n) for n in input("Input 9 numbers.").split()]
+# middle = (numbers[4])
+# print(middle*100)
+
+#Challenge 2
+
+animals = input("Input 5 ways animals can get around.").split()
+item1 = (animals[0])
+item2 = (animals[4])
+print("Some animals " + animals[0] + " and some " + animals[4])
