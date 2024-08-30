1c404eb4cf18dc9ceead566f989291eb1edf200d
Motiaa <motiaa.radwan2008@gmail.com>
2024-08-30 12:50:34 -0700

    exported from Skill Struck

---
+++ Using_Integers_from_List .py
@@ -0,0 +1,32 @@
+# lasers = [3, 10, 4, 15, 11]
+
+# print(lasers[2]*10)
+# print(round(3.14159265,3))
+
+# for x in range(0,10):
+#     print(x)
+
+# for x in range(0,10,2):
+#     print(x)
+
+
+
+
+#Challenge1
+# my_list = [int(n) for n in input().split()]
+
+# sum1 = 0
+# for num in my_list:
+#     sum1 = sum1 + num
+# print(sum1)
+
+
+
+
+#Challenge2 
+my_list = [int(n) for n in input().split()]
+
+product = 1
+for num in my_list:
+    product = product*num
+print(product)
