ebe35b751e72d1edd69b1445ca8f1162d6c628c9
Motiaa <motiaa.radwan2008@gmail.com>
2024-09-08 22:36:25 -0700

    exported from Skill Struck

---
+++ Python_Function_Parameters.py
@@ -0,0 +1,14 @@
+# def weather(type):
+#     print("It's a soggy day because it is " + type)
+# weather("raining")
+# weather("snowing")
+# weather("hialing")
+
+
+
+fruit = input("Please enter the name of a fruit: ")
+
+def hungryForApples(choice):
+    print( +  + )
+	
+()
