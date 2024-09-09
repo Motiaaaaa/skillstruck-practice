7114517a4eb52b7f4eef146dfdfaf569fe0291e0
Motiaa <motiaa.radwan2008@gmail.com>
2024-09-08 22:36:24 -0700

    exported from Skill Struck

---
+++ Python_Functions.py
@@ -0,0 +1,22 @@
+# def race():
+#     print("Bob")
+# race()
+
+
+
+# def charlie():
+#     print("Charlie is awesome!")
+
+# charlie()
+
+
+
+# start = input("Would you like to launch the game? enter yes or no")
+
+# def start_game():
+#     if start == "yes":
+#         print("Initialization Complete")
+#     else:
+#         print("Initialization Failed")
+
+# start_game()
