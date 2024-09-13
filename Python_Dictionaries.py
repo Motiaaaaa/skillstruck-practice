2b2c481247a9bc71d972e18b71cb2af11461f7c8
Motiaa <motiaa.radwan2008@gmail.com>
2024-09-13 13:58:35 -0700

    exported from Skill Struck

---
+++ Python_Dictionaries.py
@@ -0,0 +1,38 @@
+# mountains = {
+#     "Timpanogos":2,
+#     "Everest":4,
+#     "Kilimanjaro":3,
+#     "Vesuvius":1
+# }
+# print(mountains)
+# print(mountains["Vesuvius"])
+# mountains["Kilimanjaro"] = 5
+# print(mountains)
+
+
+# holidays = {
+#     "New Years": 1,
+#     "MLK Day": 2,
+#     "Washington's Birthday": 3,
+#     "Memorial Day": 4,
+#     "Independence Day": 5, 
+#     "Labor Day": 6, 
+#     "Columbus Day": 7, 
+#     "Veterans' Day": 8, 
+#     "Thanksgiving Day": 9, 
+#     "Christmas Day": 10
+# }
+# print(holidays)
+
+holidays = {
+    "New Years": 1,
+    "MLK Day": 2,
+    "Washington's Birthday": 3,
+    "Memorial Day": 4,
+    "Independence Day": 5,
+}
+print(holidays)
+
+holidays2 = ["New Years", "MLK Day", "Washington's Birthday", "Memorial Day", "Independence Day"]
+
+print(holidays2)
