a3ccf0afffe28975e32b5fa09a75aff1aa572834
Motiaa <motiaa.radwan2008@gmail.com>
2024-09-13 13:58:36 -0700

    exported from Skill Struck

---
+++ Dictionaries_Cont.py
@@ -0,0 +1,45 @@
+# coins = {
+#     "pennies": 5,
+#     "nickels": 4,
+#     "dimes": 2,
+#     "quarters": 7,
+# }
+# coins["silver dollar"] = 2
+# coins.pop("pennies")
+# print(coins)
+# print(len(coins))
+
+
+
+# dictionary = {
+# 	1: "bicycle",
+# 	2: "soccer balls",
+# 	3: "piano books"
+# }
+
+# dictionary[4] = input("What do you have 4 of?")
+# dictionary[5] = input("What do you have 5 of?")
+# dictionary[6] = input("What do you have 6 of?")
+						  
+# print(dictionary)
+
+
+
+work = {
+
+	"Monday": 3,
+	"Tuesday": 4, 
+	"Wednesday": 2,
+	"Thursday": 1, 
+	"Friday": 1, 
+	
+}
+
+work["Saturday"] = 6
+work.pop("Wednesday")
+print(len(work))
+
+if "Friday" in work:
+	print("Friday is in work")
+
+print(work)
