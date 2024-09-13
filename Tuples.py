31a0523e029f5668ad3ecb75351ba33952caa074
Motiaa <motiaa.radwan2008@gmail.com>
2024-09-13 13:58:38 -0700

    exported from Skill Struck

---
+++ Tuples.py
@@ -0,0 +1,19 @@
+# grades = (95, 70, 85, 92, 100)
+# print(grades)
+# print(grades[-2])
+# print(grades[0:3])
+
+
+# friends = (3, 5, 7, 8, 10, 2, 50, 4)
+# guess = int(input("Pick a number"))
+
+# if guess in friends:
+#     print("Yes")
+# else:
+#     print("No")
+
+
+friends = ("Bob", "Zoe", "Tim", "Kat", "Sam", "Jim", "Jon", "Kim")
+print(friends[-3])
+print(friends[5])
+print(friends[0:4])
