a8ef3f3a550f2c8638cde4e2fd16c9c6bb680fb9
Motiaa_Radwan <motiaa.radwan2008@gmail.com>
2024-10-22 12:27:01 -0700

    exported from Skill Struck

--- Python_Function_Parameters.py
+++ Python_Function_Parameters.py
@@ -5,23 +5,23 @@
 # weather("hialing")
 
 
-# fruit = input("Please enter the name of a fruit: ")
+fruit = input("Please enter the name of a fruit: ")
 
-# def hungryForApples(choice):
-#     print(fruit + fruit + fruit)
+def hungryForApples(choice):
+    print(fruit + fruit + fruit)
 	
-# hungryForApples(fruit)
+hungryForApples(fruit)
 
 
-month = int(input("Which month?  1-12"))
+# month = int(input("Which month?  1-12"))
 
-def invitation (choice) :
-	if choice == 1 or choice == 3 or choice == 5 or choice == 7 or choice == 8 or choice == 10 or choice == 12:
-		print("31")
-	elif choice == 4 or choice == 6 or choice == 9 or choice == 11:
-		print("30")
-	else:
-		print("28") 
+# def invitation (choice) :
+# 	if choice == 1 or choice == 3 or choice == 5 or choice == 7 or choice == 8 or choice == 10 or choice == 12:
+# 		print("31")
+# 	elif choice == 4 or choice == 6 or choice == 9 or choice == 11:
+# 		print("30")
+# 	else:
+# 		print("28") 
 	
 
-invitation(month)
+# invitation(month)
