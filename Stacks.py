71851080a947a65d522243897c58b5e208b8f46d
Motiaa_Radwan <motiaa.radwan2008@gmail.com>
2024-10-22 12:27:15 -0700

    exported from Skill Struck

---
+++ Stacks.py
@@ -0,0 +1,54 @@
+# stack = []
+# stack.append('Hello')
+# stack.append('my')
+# stack.append('best')
+# stack.append('friend!')
+
+# stack.pop()
+
+# print(stack)
+
+
+
+
+
+
+###Challenge 1
+# first = "r"
+
+# second = "t"
+
+# third = "s"
+
+# fourth = "y"
+
+# fifth = "o"
+
+# my_stack = []
+
+
+
+# my_stack.append(third)
+# my_stack.append(second)
+# my_stack.append(fifth)
+# my_stack.append(first)
+# my_stack.append(fourth)
+
+# print(my_stack)
+
+
+
+
+
+
+
+
+###Challenge 2
+# answer = ["apples", "steak", "potatoes", "carrots"]
+# question = input("What food would you like to add to the list?")
+
+# if "s" in question:
+#     answer.append(question)
+#     print(answer)
+# else:
+#     print("The input doesn't have the letter s")
