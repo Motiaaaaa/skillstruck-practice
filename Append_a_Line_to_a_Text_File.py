90baf809f7c523bccd8bac05f05d98db410197a2
Motiaa_Radwan <motiaa.radwan2008@gmail.com>
2024-10-22 12:27:08 -0700

    exported from Skill Struck

---
+++ Append_a_Line_to_a_Text_File.py
@@ -0,0 +1,27 @@
+# file = open("example.txt","a")
+# file.write("I love programming!")
+# file.close()
+
+# file = open("example.txt","r")
+# print(file.read())
+
+
+
+##Pen Pal
+# file = open("letter.txt","a")
+# file.write("From your Pen Pal")
+# file.close()
+
+# file = open("letter.txt","r")
+# print(file.read())
+
+
+
+
+##Give Credit
+file = open("report.txt","a")
+file.write("Quote was said by Gandhi")
+file.close()
+
+file = open("report.txt","r")
+print(file.read())
