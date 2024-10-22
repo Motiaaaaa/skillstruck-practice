8a8767410fe0314c384f66d7bc9023b92a9700df
Motiaa_Radwan <motiaa.radwan2008@gmail.com>
2024-10-22 12:27:07 -0700

    exported from Skill Struck

---
+++ Reading_Files.py
@@ -0,0 +1,13 @@
+# file = open("Reading_Files_TEXTFILE.txt","r")
+# print(file.read())
+# file.close()
+
+
+##Secret Message Challenge
+
+# file = open("cobra.txt","r")
+# print(file.read())
+
+
+##Read the Speech Challenge
+file = open("speech.txt","r")
