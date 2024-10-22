80ec90fe7946dec4a74f0d17aa209c62f9c9defb
Motiaa_Radwan <motiaa.radwan2008@gmail.com>
2024-10-22 12:27:12 -0700

    exported from Skill Struck

---
+++ Creating_an_Instance.py
@@ -0,0 +1,34 @@
+# class Hat:
+#     def __init__(self,kind, color, material):
+#         self.kind = kind
+#         self.color = color
+#         self.material = material
+# myObject = Hat("Baseball","Red","Cloth")
+
+
+###Challenge 1
+# class Fruit:
+   
+#    def __init__(self, shape, kind, size):
+#          self.shape = shape
+#          self.kind = kind
+#          self.size = size
+# f1 = Fruit("Circle","apple","small")
+# f2 = Fruit("Pear-shaped","pear","medium")
+# f3 = Fruit("Circle","orange","small")
+# f4 = Fruit("Arc","banana", "small")
+# print(f1.kind)
+# print(f2.kind)
+# print(f3.kind)
+# print(f4.kind)
+
+
+###Challenge 2
+class Pet:
+    def __init__(self,kind,fur,size):
+        self.kind = kind
+        self.fur = fur
+        self.size = size
+pet1 = Pet("cat","fluffy","small")
+pet2 = Pet("Dog","furry","big")
+pet3 = Pet("Bunny","furry","small")
